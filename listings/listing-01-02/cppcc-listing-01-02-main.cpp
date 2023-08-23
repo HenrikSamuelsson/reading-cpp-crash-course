0e992ef90404d4a0d34b6d475a0a1a1fd83411a6
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-08-23 07:01:39 +0200

    Add listing 1-2.

---
+++ listings/listing-01-02/cppcc-listing-01-02-main.cpp
@@ -0,0 +1,10 @@
+int main()
+{
+    int x = 0;
+    42 == x;  // Equality, false.
+    42 != x;  // Inequality, true.
+    100 > x;  // Greater than, true.
+    123 >= x; // Greater than or equal to, true.
+    -10 < x;  // Less than, true.
+    -99 <= x; // Less than or equal to, true.
+}
