8a715dadca8edbcce0e4be20afe358e275392995
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-08-23 07:52:55 +0200

    Editorial changes.

--- listings/listing-01-02/cppcc-listing-01-02-main.cpp
+++ listings/listing-01-02/cppcc-listing-01-02-main.cpp
@@ -1,11 +1,11 @@
 int main()
 {
     int x = 0;
-    // Cast to void to suppress warnings about the useless below expressions.
-    (void)(42 == x);  // Equality, false.
-    (void)(42 != x);  // Inequality, true.
-    (void)(100 > x);  // Greater than, true.
-    (void)(123 >= x); // Greater than or equal to, true.
-    (void)(-10 < x);  // Less than, true.
-    (void)(-99 <= x); // Less than or equal to, true.
+    // Cast to void below to suppress warnings about the useless expressions.
+    (void)(42 == x);    // Equality, false.
+    (void)(42 != x);    // Inequality, true.
+    (void)(42 > x);     // Greater than, true.
+    (void)(42 >= x);    // Greater than or equal to, true.
+    (void)(-1 < x);     // Less than, true.
+    (void)(-1 <= x);    // Less than or equal to, true.
 }
