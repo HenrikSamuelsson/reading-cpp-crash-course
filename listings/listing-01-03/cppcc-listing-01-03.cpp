fd2ba41d747e07cffddc8a905cc943a210b8159e
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-08-24 08:37:30 +0200

    Add variant of listing 1-3 taken from the book.

---
+++ listings/listing-01-03/cppcc-listing-01-03.cpp
@@ -0,0 +1,21 @@
+int main()
+{
+    bool boolean_expression_1 = false;
+    bool boolean_expression_2 = true;
+    int result = 0;
+
+    if (boolean_expression_1)
+    {
+        result = 1;
+    }
+    else if (boolean_expression_2)
+    {
+        result = 2;
+    }
+    else
+    {
+        result = 0;
+    }
+
+    return result;
+}
