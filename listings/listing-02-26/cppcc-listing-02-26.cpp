e0b43f889e74a9ec7f536278eb724e5d56ea492b
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-08-26 08:22:39 +0200

    Add variant of listing 2-26 taken from the book.

---
+++ listings/listing-02-26/cppcc-listing-02-26.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+struct Taxonomist
+{
+    Taxonomist()
+    {
+        printf("(no argument)\n");
+    }
+    Taxonomist(char x)
+    {
+        printf("char: %c\n", x);
+    }
+    Taxonomist(int x)
+    {
+        printf("int: %d\n", x);
+    }
+    Taxonomist(float x)
+    {
+        printf("float: %f\n", x);
+    }
+};
