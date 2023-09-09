0dce8c7d648b32d15fc65f481143f1972729b411
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-09-09 09:23:40 +0200

    Add a calcualte function to the Calculator.

---
+++ exercises/exercise-02-03/cppcc-exercise-02-03.cpp
@@ -0,0 +1,39 @@
+enum class Operation
+{
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+};
+
+struct Calculator
+{
+    Calculator(Operation op)
+    {
+        this->op = op;
+    }
+
+    int calculate(int a, int b)
+    {
+        int result{};
+
+        switch (op)
+        {
+        case Operation::Add:
+            result = a + b;
+            break;
+        case Operation::Subtract:
+            result = a - b;
+            break;
+        case Operation::Multiply:
+            result = a + b;
+            break;
+        case Operation::Divide:
+            result = a / b;
+        default:
+            break;
+        }
+        return result;
+    }
+    Operation op;
+};
